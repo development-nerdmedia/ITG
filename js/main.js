8011d98a590aebf7bfc6ae04b66ab94af16b5be9
Diego Castillo <88866352+diegocastillosaldana@users.noreply.github.com>
2022-06-23 17:11:56 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -33,20 +33,7 @@ MyApp = {
                     } else {
                         document.querySelector(".formname").classList.add("ok");
                     }
-                }
-                if (e.target.closest(".formaempresa")) {
-                    document.querySelector(".formaempresa").classList.add("focusin");
-                    document.querySelector(".formaempresa span").classList.add("animacion");
-                } else {
-                    document.querySelector(".formaempresa").classList.remove("focusin");
-                    email = document.getElementById('name-empresa').value;
-                    document.querySelector(".formaempresa span").classList.remove("animacion");
-                    if (!email) {
-                        document.querySelector(".formaempresa").classList.remove("ok");
-                    } else {
-                        document.querySelector(".formaempresa").classList.add("ok");
-                    }
-                }
+                }                
                 if (e.target.closest(".formcorreo")) {
                     document.querySelector(".formcorreo").classList.add("focusin");
                     document.querySelector(".formcorreo span").classList.add("animacion");
@@ -73,17 +60,92 @@ MyApp = {
                         document.querySelector(".formtel").classList.add("ok");
                     }
                 }
+                if (e.target.closest(".formaempresa")) {
+                    document.querySelector(".formaempresa").classList.add("focusin");
+                    document.querySelector(".formaempresa span").classList.add("animacion");
+                } else {
+                    document.querySelector(".formaempresa").classList.remove("focusin");
+                    email = document.getElementById('name-empresa').value;
+                    document.querySelector(".formaempresa span").classList.remove("animacion");
+                    if (!email) {
+                        document.querySelector(".formaempresa").classList.remove("ok");
+                    } else {
+                        document.querySelector(".formaempresa").classList.add("ok");
+                    }
+                }
             })
         }
-    }  
+    },
+    trabajolabelcontactopage: {
+        init: function () {
+            document.addEventListener("click", function (e) {
+                if (e.target.closest(".formname")) {
+                    document.querySelector(".formname").classList.add("focusin");
+                    document.querySelector(".formname span").classList.add("animacion");
+                } else {
+                    document.querySelector(".formname").classList.remove("focusin");
+                    nombre = document.getElementById('name').value;
+                    document.querySelector(".formname span").classList.remove("animacion");
+                    if (!nombre) {
+                        document.querySelector(".formname").classList.remove("ok");
+                    } else {
+                        document.querySelector(".formname").classList.add("ok");
+                    }
+                }                
+                if (e.target.closest(".formcorreo")) {
+                    document.querySelector(".formcorreo").classList.add("focusin");
+                    document.querySelector(".formcorreo span").classList.add("animacion");
+                } else {
+                    document.querySelector(".formcorreo").classList.remove("focusin");
+                    asunto = document.getElementById('email').value;
+                    document.querySelector(".formcorreo span").classList.remove("animacion");
+                    if (!asunto) {
+                        document.querySelector(".formcorreo").classList.remove("ok");
+                    } else {
+                        document.querySelector(".formcorreo").classList.add("ok");
+                    }
+                }
+                if (e.target.closest(".formtel")) {
+                    document.querySelector(".formtel").classList.add("focusin");
+                    document.querySelector(".formtel span").classList.add("animacion");
+                } else {
+                    document.querySelector(".formtel").classList.remove("focusin");
+                    textarea = document.getElementById('telephone').value;
+                    document.querySelector(".formtel span").classList.remove("animacion");
+                    if (!textarea) {
+                        document.querySelector(".formtel").classList.remove("ok");
+                    } else {
+                        document.querySelector(".formtel").classList.add("ok");
+                    }
+                }
+                if (e.target.closest(".formmensaje")) {
+                    document.querySelector(".formmensaje").classList.add("focusin");
+                    document.querySelector(".formmensaje span").classList.add("animacion");
+                } else {
+                    document.querySelector(".formmensaje").classList.remove("focusin");
+                    email = document.getElementById('mensaje').value;
+                    document.querySelector(".formmensaje span").classList.remove("animacion");
+                    if (!email) {
+                        document.querySelector(".formmensaje").classList.remove("ok");
+                    } else {
+                        document.querySelector(".formmensaje").classList.add("ok");
+                    }
+                }
+            })
+        }
+    } 
 }
 
 if ($('header').length > 0) {
     MyApp.scroll.init();
 }
 
-if ($('.formulario').length > 0) {
+if ($('.home-servicios-interna').length > 0) {
     MyApp.labelcontactopage.init();
+}
+
+if ($('.trabajo').length > 0) {
+    MyApp.trabajolabelcontactopage.init();
 }
 
 $('.slider-novedades').slick({
