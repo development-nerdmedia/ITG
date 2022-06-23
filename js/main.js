6019878d31f7aed12179262541d89f10e497eebf
Diego Castillo <88866352+diegocastillosaldana@users.noreply.github.com>
2022-06-23 11:05:18 -0500

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -144,7 +144,7 @@ $('.slider-competencias').slick({
     infinite: true,
     speed: 300,
     dots: false,
-    // autoplay: true,    
+    autoplay: true,    
     slidesToShow: 3,
     slidesToScroll: 1,
     responsive: [
